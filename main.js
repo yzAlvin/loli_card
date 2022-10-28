23d4bb7064d6a656ab9b60cd43a1f288ea3febcc
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2022-10-28 21:51:45 +1100

    loli card first cut

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 300,
+    icon: 'build/loli.png',
+    autoHideMenuBar: true,
+    minimizable: false,
+    maximizable: false,
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
